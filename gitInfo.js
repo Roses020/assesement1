895b5774e9c1d630deb134dfb83f1494914633db
aimee ray <aimeeray3@gmail.com>
2022-10-21 12:52:27 -0600

    test

---
+++ gitInfo.js
@@ -0,0 +1,80 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+const gitDefinition = "Git is used to tracking changes in the sourcecode and management. it tracks changes in any set of files"
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+const gitHubDefinition = " it lets you and others work on projects from anywhere and lets you store your projects. also so others can have acces to your projects"
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+const gitInitDefinition =" can be used to convert an existing project to a Git repository or initialize a new, empty repository."
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+const gitCloneDefinition = 'is used to point to an exiciting repo and make a clone or copy of that repo in a new directory'
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+const gitStatusDefinition ="displays the state of the working directory and the staging area"
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+const gitAddDefinition = " It tells Git that you want to include updates to a particular file in the next commit"
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+
+const gitCommitDefinition ="The git commit command saves all currently staged changes of the project."
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
---
+++ sdlc.js
@@ -0,0 +1,32 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+planning
+Analysis
+design
+implementaions
+testing
+maintenance
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const Analysis =" The analysis stage involves defining project goals as functions. "
+ const design =" Is an essential phase of the Software Development Life Cycle. The list of requirements that you develop in the definition phase is used to make design choices. In the design phase, one or more designs are created to achieve the project result"
+const implementaions = "This phase details the who, what, when and how much of your communication strategy. The plan covers partner roles and responsibilities, activities, timeline, budget and management considerations. "
+const testing = "Is a process used to test software and ensure that quality standards are met. Tests are carried out systematically over several phases"
+const maintenance = "Its main purpose is to modify and update software application after delivery to correct faults and to improve performance."
+
